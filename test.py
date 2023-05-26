72af27764e460460af3cf55086a5ac2645f2dc12
LEEJAECHUN <121738554+LJC0831@users.noreply.github.com>
2023-05-27 00:58:16 +0900

    Update test.py

--- test.py
+++ test.py
@@ -49,21 +49,48 @@ log_ENT.click()
 #i = 0
 #while i == 0 :
     
-reserv_url = "https://booking.naver.com/booking/3/bizes/2066/items/4313744?startDate=2023-07-07&endDate=2023-07-09"
-driver.get(reserv_url)
-time.sleep(1)
+i = 0
+while i < 1 : 
+    #reserv_url = "https://booking.naver.com/booking/3/bizes/2066/items/4313744?startDate=2023-07-07&endDate=2023-07-09"
+    reserv_url = "https://booking.naver.com/booking/3/bizes/2066/items/4313744?startDate=2023-06-29&endDate=2023-06-30"
+    driver.get(reserv_url)
+    time.sleep(1)
 
-#다음단계버튼
-next_btn = driver.find_element(By.XPATH, '//*[@id="container"]/bk-accommodation/div/div/div[2]/bk-submit/div/button')
-next_btn.click() 
+    #다음단계버튼
+    next_btn = driver.find_element(By.XPATH, '//*[@id="container"]/bk-accommodation/div/div/div[2]/bk-submit/div/button')
+    next_btn.click() 
 
-# 예약가능여부
-use_yn = driver.find_element(By.XPATH, '/html/body/app/bk-alert/div/div[2]').is_displayed()
+    # 예약가능여부
+    use_yn = driver.find_element(By.XPATH, '/html/body/app/bk-alert/div/div[2]').is_displayed()
+    if use_yn : 
+        print('다시체크')
+    else :
+        first_memo = driver.find_element(By.XPATH, '//*[@id="container"]/bk-accommodation/div/div/div[2]/div[2]/div[1]/bk-extra-input/div/div/form/div[1]/div/div/div/div/div[1]')
+        first_memo.click()
+        first_memo01 = driver.find_element(By.XPATH, '//*[@id="container"]/bk-accommodation/div/div/div[2]/div[2]/div[1]/bk-extra-input/div/div/form/div[1]/div/div/div/div/div[2]/div/ul/li[2]')
+        first_memo01.click()
+        second_memo = driver.find_element(By.XPATH, '//*[@id="container"]/bk-accommodation/div/div/div[2]/div[2]/div[1]/bk-extra-input/div/div/form/div[2]/div/div/div/div/div[1]')
+        second_memo.click() 
+        second_memo01 = driver.find_element(By.XPATH, '//*[@id="container"]/bk-accommodation/div/div/div[2]/div[2]/div[1]/bk-extra-input/div/div/form/div[2]/div/div/div/div/div[2]/div/ul/li[2]')
+        second_memo01.click() 
+        third_memo = driver.find_element(By.XPATH, '//*[@id="container"]/bk-accommodation/div/div/div[2]/div[2]/div[1]/bk-extra-input/div/div/form/div[3]/div/div/div/div/div[1]')
+        third_memo.click() 
+        third_memo01 = driver.find_element(By.XPATH, '//*[@id="container"]/bk-accommodation/div/div/div[2]/div[2]/div[1]/bk-extra-input/div/div/form/div[3]/div/div/div/div/div[2]/div/ul/li[2]')
+        third_memo01.click() 
+        forth_memo = driver.find_element(By.XPATH, '//*[@id="container"]/bk-accommodation/div/div/div[2]/div[2]/div[1]/bk-extra-input/div/div/form/div[4]/div/div/div/div/div[1]')
+        forth_memo.click() 
+        forth_memo01 = driver.find_element(By.XPATH, '//*[@id="container"]/bk-accommodation/div/div/div[2]/div[2]/div[1]/bk-extra-input/div/div/form/div[4]/div/div/div/div/div[2]/div/ul/li[2]')
+        forth_memo01.click() 
 
-if use_yn : 
-    print('다시체크')
-else:
-    print('예약가능')
-   
+        driver.find_element(By.XPATH, '//*[@id="name"]').click() 
+        driver.find_element(By.XPATH, '//*[@id="tel"]').click() 
+        driver.find_element(By.XPATH, '//*[@id="email"]').click() 
+        driver.find_element(By.XPATH, '//*[@id="message"]').click() 
+        #예약하기 버튼 주석풀면 예약됨
+        #driver.find_element(By.XPATH, '//*[@id="container"]/bk-accommodation/div/div/div[2]/bk-submit/div/button/span').click()  
+        break
+    
+    
+        
 
-
+    
