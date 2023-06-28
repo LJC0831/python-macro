f1a5ad9abf982e9899969f2f24047b91ca3c281e
LEEJAECHUN <121738554+LJC0831@users.noreply.github.com>
2023-06-28 18:11:07 +0900

    Update coopang.py

--- coopang.py
+++ coopang.py
@@ -11,47 +11,81 @@ from selenium.webdriver.common.alert import Alert
 import easyocr
 import keyboard
 seat = 0
+def check_go2(seatText):
+    if (seatText == "A") :
+        driver.find_element(By.XPATH, seatText).click()  # 예약버튼
+        WebDriverWait(driver, 300).until(EC.presence_of_element_located((By.XPATH, '//*[@id="seat_grade_83445"]/div/div/span[1]/a')))
+        driver.find_element(By.XPATH, '//*[@id="seat_grade_83445"]/div/div/span[1]/a').click()  #
+        WebDriverWait(driver, 300).until(EC.presence_of_element_located((By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[1]/form/fieldset/div/a')))
+        driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[1]/form/fieldset/div/a').send_keys('2')
+        WebDriverWait(driver, 300).until(EC.presence_of_element_located((By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[2]/a')))
+        driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[2]/a').click()  #
+    
+    if (seatText == "B") :
+        driver.find_element(By.XPATH, seatText).click()  # 예약버튼
+        WebDriverWait(driver, 300).until(EC.presence_of_element_located((By.XPATH, '//*[@id="seat_grade_83446"]/div/div/span[1]/a')))
+        driver.find_element(By.XPATH, '//*[@id="seat_grade_83446"]/div/div/span[1]/a').click()  #
+        WebDriverWait(driver, 300).until(EC.presence_of_element_located((By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[1]/form/fieldset/div/a')))
+        driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[1]/form/fieldset/div/a').send_keys('2')
+        WebDriverWait(driver, 300).until(EC.presence_of_element_located((By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[2]/a')))
+        driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[2]/a').click()  #
+    
+    if (seatText == "C") :
+        driver.find_element(By.XPATH, seatText).click()  # 예약버튼
+        WebDriverWait(driver, 300).until(EC.presence_of_element_located((By.XPATH, '//*[@id="seat_grade_83447"]/div/div/span[1]/a')))
+        driver.find_element(By.XPATH, '//*[@id="seat_grade_83447"]/div/div/span[1]/a').click()  #
+        WebDriverWait(driver, 300).until(EC.presence_of_element_located((By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[1]/form/fieldset/div/a')))
+        driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[1]/form/fieldset/div/a').send_keys('2')
+        WebDriverWait(driver, 300).until(EC.presence_of_element_located((By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[2]/a')))
+        driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[2]/a').click()  #
+        
+    
 
 def check_go(seatName, i):
     if (seatName >= 2) :
                 
                 if (i == "STA") :
                     print("1등석A좌석 취소티켓발생")
+                    check_go2("A")
                 if (i == "MID") :
                     print("1등석B좌석 취소티켓발생")
+                    check_go2("B")
                 if (i == "END") :
                     print("1등석C좌석 취소티켓발생")
+                    check_go2("C")
                 # 알림 생성을 위한 자바스크립트 실행 예제
+                
+                
                 driver.execute_script("alert('취소티켓발생');")
-                each = 0
-                while each < 1 :
-                    print("시간 | 좌석선택완료 버튼 좌표 가져오기\n 마우스 가져다 대고 순서대로 1, 2 를 누르면 됨\n")
-                    k = 0
-                    while k < 1 :
-                        if keyboard.read_key() == "1":
-                            xxx, yyy = pyautogui.position()
-                            break
-                    l = 0
-                    pyautogui.moveTo(xxx , yyy , 0.01) # 좌석상세 마우스이동
-                    pyautogui.click()
-                    while l < 1 :
-                        if keyboard.read_key() == "2":
-                            selx, sely = pyautogui.position()
-                            break
-                    pyautogui.moveTo(selx , sely , 0.01) # 좌석상세 마우스이동
-                    pyautogui.click()
-                    driver.switch_to.window(driver.window_handles[-1])
-                    driver.switch_to.frame(driver.find_element(By.XPATH, '//*[@id="marketing-webview"]')) # iframe 이동
-                    driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[4]/a').click()  # 예약버튼
-                    try :
-                        da = Alert(driver)
-                        da.accept()
-                        time.sleep(1)
-                    except :
-                        print("좌석선택완료")
+                # each = 0
+                # while each < 1 :
+                #     print("시간 | 좌석선택완료 버튼 좌표 가져오기\n 마우스 가져다 대고 순서대로 1, 2 를 누르면 됨\n")
+                #     k = 0
+                #     while k < 1 :
+                #         if keyboard.read_key() == "1":
+                #             xxx, yyy = pyautogui.position()
+                #             break
+                #     l = 0
+                #     pyautogui.moveTo(xxx , yyy , 0.01) # 좌석상세 마우스이동
+                #     pyautogui.click()
+                #     while l < 1 :
+                #         if keyboard.read_key() == "2":
+                #             selx, sely = pyautogui.position()
+                #             break
+                #     pyautogui.moveTo(selx , sely , 0.01) # 좌석상세 마우스이동
+                #     pyautogui.click()
+                #     driver.switch_to.window(driver.window_handles[-1])
+                #     driver.switch_to.frame(driver.find_element(By.XPATH, '//*[@id="marketing-webview"]')) # iframe 이동
+                #     driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[4]/a').click()  # 예약버튼
+                #     try :
+                #         da = Alert(driver)
+                #         da.accept()
+                #         time.sleep(1)
+                #     except :
+                #         print("좌석선택완료")
 
-                    if keyboard.read_key() == "`":
-                        break
+                #     if keyboard.read_key() == "`":
+                #         break
     else :
         if (i == "STA") :
             print("1등석A좌석없음")
@@ -124,7 +158,6 @@ while i < 1 :
             seat01 = int(driver.find_element(By.XPATH, '//*[@id="seat_grade_83445"]/a/div/span[2]/span[1]').text)
             seat02 = int(driver.find_element(By.XPATH, '//*[@id="seat_grade_83446"]/a/div/span[2]/span[1]').text)
             seat03 = int(driver.find_element(By.XPATH, '//*[@id="seat_grade_83447"]/a/div/span[2]/span[1]').text)
-            
             check_go(seat01, "STA")
             check_go(seat02, "MID")
             check_go(seat03, "END")
