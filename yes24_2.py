1b58c8c5cc3778ea9d66a562601047bbf9d267e9
LEEJAECHUN <121738554+LJC0831@users.noreply.github.com>
2023-07-04 01:03:19 +0900

    Update yes24_2.py

--- yes24_2.py
+++ yes24_2.py
@@ -16,8 +16,6 @@ import os
 
 mp3_file = "C:/Users/dlwoc/OneDrive/Desktop/aa.mp3"
 
-def play_mp3(file_path):
-    os.startfile(file_path)
 if ctypes.windll.shell32.IsUserAnAdmin(): 
     print('관리자권한으로 실행된 프로세스입니다.')
 else:
@@ -29,7 +27,118 @@ chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 #yes24 이동
-url = "http://ticket.yes24.com/Special/46234" 
+url = "http://ticket.yes24.com/Special/46234"
+
+def play_mp3(file_path):
+    os.startfile(file_path)
+
+def macro():
+    i = 0
+    while i==0 :
+        driver.switch_to.default_content()
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="2023-10-20"]'))) #페이지로딩(최대300초)
+        driver.find_element(By.XPATH, '//*[@id="2023-10-20"]').click()
+        time.sleep(0.5)
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="btnSeatSelect"]'))) #페이지로딩(최대300초)
+        driver.find_element(By.XPATH, '//*[@id="btnSeatSelect"]').click()
+        time.sleep(0.5)
+        driver.switch_to.window(driver.window_handles[-1])
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="divFlash"]/iframe'))) #페이지로딩(최대300초)
+        driver.switch_to.frame(driver.find_element(By.XPATH, '//*[@id="divFlash"]/iframe')) # iframe 이동
+        seat_array = []
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_P석"]/em'))) #페이지로딩(최대300초)
+        seat_array.append(driver.find_element(By.XPATH, '//*[@id="grade_P석"]/em').text)
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_R석"]/em'))) #페이지로딩(최대300초)
+        seat_array.append(driver.find_element(By.XPATH, '//*[@id="grade_R석"]/em').text)
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_스탠딩석"]/em'))) #페이지로딩(최대300초)
+        seat_array.append(driver.find_element(By.XPATH, '//*[@id="grade_스탠딩석"]/em').text)
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_S석"]/em'))) #페이지로딩(최대300초)
+        seat_array.append(driver.find_element(By.XPATH, '//*[@id="grade_S석"]/em').text)
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_A석"]/em'))) #페이지로딩(최대300초)
+        seat_array.append(driver.find_element(By.XPATH, '//*[@id="grade_A석"]/em').text)
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_B석"]/em'))) #페이지로딩(최대300초)
+        seat_array.append(driver.find_element(By.XPATH, '//*[@id="grade_B석"]/em').text)
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_C석"]/em'))) #페이지로딩(최대300초)
+        seat_array.append(driver.find_element(By.XPATH, '//*[@id="grade_C석"]/em').text)
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_D석"]/em'))) #페이지로딩(최대300초)
+        seat_array.append(driver.find_element(By.XPATH, '//*[@id="grade_D석"]/em').text)
+
+        for z in seat_array :
+            if(z == "(0석)") :
+                continue
+            else :
+                play_mp3(mp3_file)
+                print("금요일 좌석발생")
+                seat = 0
+                while seat < 1 :
+                    print("매크로재시작 ` 키")
+                    k = 0
+                    while k < 1 :
+                        if keyboard.read_key() == "`":
+                            macro()
+                            break
+                
+        
+        print("금요일 좌석 현재없음")
+
+        # 토요일
+        #time.sleep(1)
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="form1"]/div[3]/div[2]/div/div[2]/p[1]/a[1]/img'))) #페이지로딩(최대300초)
+        driver.find_element(By.XPATH, '//*[@id="form1"]/div[3]/div[2]/div/div[2]/p[1]/a[1]/img').click()
+        #time.sleep(0.5)
+        driver.switch_to.default_content()
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="2023-10-21"]'))) #페이지로딩(최대300초)
+        driver.find_element(By.XPATH, '//*[@id="2023-10-21"]').click()
+        time.sleep(0.5)
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="btnSeatSelect"]'))) #페이지로딩(최대300초)
+        driver.find_element(By.XPATH, '//*[@id="btnSeatSelect"]').click()
+        time.sleep(0.5)
+        driver.switch_to.window(driver.window_handles[-1])
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="divFlash"]/iframe'))) #페이지로딩(최대300초)
+        driver.switch_to.frame(driver.find_element(By.XPATH, '//*[@id="divFlash"]/iframe')) # iframe 이동
+
+        seat_array2 = []
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_P석"]/em'))) #페이지로딩(최대300초)
+        seat_array2.append(driver.find_element(By.XPATH, '//*[@id="grade_P석"]/em').text)
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_R석"]/em'))) #페이지로딩(최대300초)
+        seat_array2.append(driver.find_element(By.XPATH, '//*[@id="grade_R석"]/em').text)
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_스탠딩석"]/em'))) #페이지로딩(최대300초)
+        seat_array2.append(driver.find_element(By.XPATH, '//*[@id="grade_스탠딩석"]/em').text)
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_S석"]/em'))) #페이지로딩(최대300초)
+        seat_array2.append(driver.find_element(By.XPATH, '//*[@id="grade_S석"]/em').text)
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_A석"]/em'))) #페이지로딩(최대300초)
+        seat_array2.append(driver.find_element(By.XPATH, '//*[@id="grade_A석"]/em').text)
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_B석"]/em'))) #페이지로딩(최대300초)
+        seat_array2.append(driver.find_element(By.XPATH, '//*[@id="grade_B석"]/em').text)
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_C석"]/em'))) #페이지로딩(최대300초)
+        seat_array2.append(driver.find_element(By.XPATH, '//*[@id="grade_C석"]/em').text)
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_D석"]/em'))) #페이지로딩(최대300초)
+        seat_array2.append(driver.find_element(By.XPATH, '//*[@id="grade_D석"]/em').text)
+
+        for x in seat_array :
+            if(x == "(0석)") :
+                continue
+            else :
+                play_mp3(mp3_file)
+                print("토요일 좌석발생")
+                seat = 0
+                while seat < 1 :
+                    print("매크로재시작 ` 키")
+                    k = 0
+                    while k < 1 :
+                        if keyboard.read_key() == "`":
+                            macro()
+                            break
+
+        print("토요일 좌석 현재없음")
+        driver.switch_to.window(driver.window_handles[-1])
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="divFlash"]/iframe'))) #페이지로딩(최대300초)
+        driver.switch_to.frame(driver.find_element(By.XPATH, '//*[@id="divFlash"]/iframe')) # iframe 이동
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="form1"]/div[3]/div[2]/div/div[2]/p[1]/a[1]/img'))) #페이지로딩(최대300초)
+        driver.find_element(By.CSS_SELECTOR, '#form1 > div.bx_seatbg > div.seatinfo > div > div.btn > p:nth-child(1) > a:nth-child(1) > img').click()
+
+    
+ 
 driver.get(url)
 
 #로그인
@@ -44,96 +153,15 @@ WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@
 driver.find_element(By.XPATH, '//*[@id="mainForm"]/div[9]/div/div[4]/a[4]').click()
 driver.switch_to.window(driver.window_handles[-1])
 
+try :
+    macro()
+except :
+    seat = 0
+    while seat < 1 :
+        print("매크로재시작 ` 키")
+        k = 0
+        while k < 1 :
+            if keyboard.read_key() == "`":
+                macro()
+                break
 
-i = 0
-while i==0 :
-    driver.switch_to.default_content()
-    time.sleep(1)
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="2023-10-20"]'))) #페이지로딩(최대300초)
-    driver.find_element(By.XPATH, '//*[@id="2023-10-20"]').click()
-    time.sleep(1)
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="btnSeatSelect"]'))) #페이지로딩(최대300초)
-    driver.find_element(By.XPATH, '//*[@id="btnSeatSelect"]').click()
-
-    time.sleep(1)
-    driver.switch_to.window(driver.window_handles[-1])
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="divFlash"]/iframe'))) #페이지로딩(최대300초)
-    driver.switch_to.frame(driver.find_element(By.XPATH, '//*[@id="divFlash"]/iframe')) # iframe 이동
-    seat_array = []
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_P석"]/em'))) #페이지로딩(최대300초)
-    seat_array.append(driver.find_element(By.XPATH, '//*[@id="grade_P석"]/em').text)
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_R석"]/em'))) #페이지로딩(최대300초)
-    seat_array.append(driver.find_element(By.XPATH, '//*[@id="grade_R석"]/em').text)
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_스탠딩석"]/em'))) #페이지로딩(최대300초)
-    seat_array.append(driver.find_element(By.XPATH, '//*[@id="grade_스탠딩석"]/em').text)
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_S석"]/em'))) #페이지로딩(최대300초)
-    seat_array.append(driver.find_element(By.XPATH, '//*[@id="grade_S석"]/em').text)
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_A석"]/em'))) #페이지로딩(최대300초)
-    seat_array.append(driver.find_element(By.XPATH, '//*[@id="grade_A석"]/em').text)
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_B석"]/em'))) #페이지로딩(최대300초)
-    seat_array.append(driver.find_element(By.XPATH, '//*[@id="grade_B석"]/em').text)
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_C석"]/em'))) #페이지로딩(최대300초)
-    seat_array.append(driver.find_element(By.XPATH, '//*[@id="grade_C석"]/em').text)
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_D석"]/em'))) #페이지로딩(최대300초)
-    seat_array.append(driver.find_element(By.XPATH, '//*[@id="grade_D석"]/em').text)
-
-    for z in seat_array :
-        if(z == "(0석)") :
-            continue
-        else :
-            play_mp3(mp3_file)
-            print("금요일 좌석발생")
-            i = 10
-            break
-    
-    print("금요일 좌석 현재없음")
-
-    # 토요일
-    time.sleep(1)
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="form1"]/div[3]/div[2]/div/div[2]/p[1]/a[1]/img'))) #페이지로딩(최대300초)
-    driver.find_element(By.XPATH, '//*[@id="form1"]/div[3]/div[2]/div/div[2]/p[1]/a[1]/img').click()
-    time.sleep(1)
-    driver.switch_to.default_content()
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="2023-10-21"]'))) #페이지로딩(최대300초)
-    driver.find_element(By.XPATH, '//*[@id="2023-10-21"]').click()
-    time.sleep(1)
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="btnSeatSelect"]'))) #페이지로딩(최대300초)
-    driver.find_element(By.XPATH, '//*[@id="btnSeatSelect"]').click()
-    time.sleep(1)
-    driver.switch_to.window(driver.window_handles[-1])
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="divFlash"]/iframe'))) #페이지로딩(최대300초)
-    driver.switch_to.frame(driver.find_element(By.XPATH, '//*[@id="divFlash"]/iframe')) # iframe 이동
-
-    seat_array2 = []
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_P석"]/em'))) #페이지로딩(최대300초)
-    seat_array2.append(driver.find_element(By.XPATH, '//*[@id="grade_P석"]/em').text)
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_R석"]/em'))) #페이지로딩(최대300초)
-    seat_array2.append(driver.find_element(By.XPATH, '//*[@id="grade_R석"]/em').text)
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_스탠딩석"]/em'))) #페이지로딩(최대300초)
-    seat_array2.append(driver.find_element(By.XPATH, '//*[@id="grade_스탠딩석"]/em').text)
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_S석"]/em'))) #페이지로딩(최대300초)
-    seat_array2.append(driver.find_element(By.XPATH, '//*[@id="grade_S석"]/em').text)
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_A석"]/em'))) #페이지로딩(최대300초)
-    seat_array2.append(driver.find_element(By.XPATH, '//*[@id="grade_A석"]/em').text)
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_B석"]/em'))) #페이지로딩(최대300초)
-    seat_array2.append(driver.find_element(By.XPATH, '//*[@id="grade_B석"]/em').text)
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_C석"]/em'))) #페이지로딩(최대300초)
-    seat_array2.append(driver.find_element(By.XPATH, '//*[@id="grade_C석"]/em').text)
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="grade_D석"]/em'))) #페이지로딩(최대300초)
-    seat_array2.append(driver.find_element(By.XPATH, '//*[@id="grade_D석"]/em').text)
-
-    for x in seat_array :
-        if(x == "(0석)") :
-            continue
-        else :
-            play_mp3(mp3_file)
-            print("토요일 좌석발생")
-            i = 10
-            break
-
-    print("토요일 좌석 현재없음")
-    driver.switch_to.window(driver.window_handles[-1])
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="divFlash"]/iframe'))) #페이지로딩(최대300초)
-    driver.switch_to.frame(driver.find_element(By.XPATH, '//*[@id="divFlash"]/iframe')) # iframe 이동
-    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="form1"]/div[3]/div[2]/div/div[2]/p[1]/a[1]/img'))) #페이지로딩(최대300초)
-    driver.find_element(By.CSS_SELECTOR, '#form1 > div.bx_seatbg > div.seatinfo > div > div.btn > p:nth-child(1) > a:nth-child(1) > img').click()
