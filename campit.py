0948718831b1a58e43e9f4bb6b7df29ace9c2f4d
LEEJAECHUN <121738554+LJC0831@users.noreply.github.com>
2024-07-15 00:06:59 +0900

    Update campit.py

--- campit.py
+++ campit.py
@@ -18,8 +18,8 @@ def macro01():
 
     try :
         #사이트이동
-        #url = "https://camfit.co.kr/camp/654881e9d7262a001eebf044/654886e8d7262a001eebf045" #집밖으로
-        url = "https://camfit.co.kr/camp/6446197cd3fd8c001e52ce80/644619c62eb46a001e5bcf89" #test
+        url = "https://camfit.co.kr/camp/654881e9d7262a001eebf044/654886e8d7262a001eebf045" #집밖으로
+        #url = "https://camfit.co.kr/camp/6446197cd3fd8c001e52ce80/644619c62eb46a001e5bcf89" #test
         driver.get(url)
         time.sleep(0.5)
 
@@ -28,49 +28,59 @@ def macro01():
         while each < 1 :
                 if keyboard.read_key() == "`":
                     break
-        time.sleep(0.5)
-        WebDriverWait(driver, 300).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/section/div[1]/div[8]/div/div/button'))) #
-        driver.find_element(By.XPATH, '//*[@id="root"]/div/section/div[1]/div[8]/div/div/button').click()  #
-        time.sleep(0.5)
-        
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/section/div[1]/div[10]'))) #
+        driver.find_element(By.XPATH, '//*[@id="root"]/div/section/div[1]/div[10]').click()  #
         try :
-            driver.find_element(By.XPATH, '/html/body/div[4]/div[3]/div/div[3]/div[1]/div[1]/button[2]').click() # 다음달
-            time.sleep(0.5)
-        except :
-            pyautogui.moveTo(1415 , 505 , 0.01) # 좌석상세 마우스이동
+            WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div[3]/div/div[3]/div[1]/div[1]/button[2]'))) #
+            pyautogui.moveTo(1723 , 1019 , 0.01) # 좌석상세 마우스이동
             time.sleep(0.1)
             pyautogui.click()
-                   
-        WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div[3]/div/div[3]/div[1]/div[5]/div[1]/div[5]/div[2]'))) #
-        sta_ymd = driver.find_element(By.XPATH, '/html/body/div[4]/div[3]/div/div[3]/div[1]/div[5]/div[2]/div[2]/div[6]') #달력 8일 나중에 수정#####################
-        sta_ymd.click() 
-        end_ymd = driver.find_element(By.XPATH, '/html/body/div[4]/div[3]/div/div[3]/div[1]/div[5]/div[2]/div[3]/div[1]') #달력 10일 나중에 수정#####################
-        end_ymd.click() 
-
-        driver.find_element(By.XPATH, '/html/body/div[4]/div[3]/div/button').click() # 선택완료
-        time.sleep(0.5)
-        WebDriverWait(driver, 300).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div[3]/div/button'))) #
-        driver.find_element(By.XPATH, '/html/body/div[4]/div[3]/div/button').click() # 선택완료2
-        time.sleep(0.5)
-        try :
-            driver.find_element(By.XPATH, '//*[@id="root"]/div/section/div[1]/div[8]/div/div[2]/div[2]/div[2]').click() # 자리 나중에 수정#####################
+            time.sleep(0.5)
+            WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div[3]/div/div[3]/div[1]/div[5]/div[2]/div[1]/div[6]'))) #
+            pyautogui.moveTo(1633 , 1115 , 0.01) # 시작일
+            time.sleep(0.1)
+            pyautogui.click()
+            pyautogui.moveTo(1277 , 1155 , 0.01) # 종료일
+            time.sleep(0.1)
+            pyautogui.click()
+            driver.find_element(By.XPATH, '/html/body/div[3]/div[3]/div/button').click() # 선택완료
         except :
-            print('자리없음')
+             macro01()
             
-        time.sleep(0.5)
-        #사이트예약하기 버튼
+        
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[3]/div[3]/div/button'))) #
+        pyautogui.moveTo(1500 , 1363 , 0.01) # 2명 선택완료
+        time.sleep(0.1)
+        pyautogui.click()
+        # time.sleep(0.5)
+        # try :
+        #     WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/section/div[1]/div[8]/div/div[2]/div[2]/div[1]'))) #
+        #     driver.find_element(By.XPATH, '//*[@id="root"]/div/section/div[1]/div[8]/div/div[2]/div[2]/div[1]').click() # 자리 나중에 수정#####################
+        # except :
+        #     print('자리없음')
+
+        each2 = 0
+        print("자리선택 완료후 ` 눌러")
+        while each2 < 1 :
+                if keyboard.read_key() == "`":
+                    break
+            
+        # #사이트예약하기 버튼
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/section/div[1]/div[10]/button'))) #
         driver.find_element(By.XPATH, '//*[@id="root"]/div/section/div[1]/div[10]/button').click() 
-        time.sleep(0.5)
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/section/div[1]/div[11]/div[2]/button'))) #
         driver.find_element(By.XPATH, '//*[@id="root"]/div/section/div[1]/div[11]/div[2]/button').click()  #확인하고 예약계속
-        time.sleep(0.5) 
-        # ID,PW
+        
+        # # ID,PW
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/section/div[1]/div[2]/div[2]/input[1]'))) #
         driver.find_element(By.XPATH, '//*[@id="root"]/div/section/div[1]/div[2]/div[2]/input[1]').send_keys('leejc831')
         driver.find_element(By.XPATH, '//*[@id="root"]/div/section/div[1]/div[2]/div[2]/input[2]').send_keys('qrg258*01')
-        driver.find_element(By.XPATH, '//*[@id="root"]/div/section/div[1]/div[2]/button').click()
+        driver.find_element(By.XPATH, '//*[@id="root"]/div/section/div[1]/div[2]/button/span[1]').click()
         time.sleep(100000)
         #driver.find_element(By.XPATH, '//*[@id="root"]/div/section/div[1]/div[2]/button').click() #예약하기 버튼.. 우선 주석
     # 예약가능여부
     except:
+        print('에러')
         macro01()
 
 
