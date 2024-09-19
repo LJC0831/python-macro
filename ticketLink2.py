37057f60de7e7f46914c1caf99dfb609e7d0069d
LEEJAECHUN <121738554+LJC0831@users.noreply.github.com>
2024-09-19 23:37:04 +0900

    Update ticketLink2.py

--- ticketLink2.py
+++ ticketLink2.py
@@ -28,52 +28,76 @@ def check():
         
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[2]/div[1]/div[2]/a'))) #새로고침
         driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[2]/div[1]/div[2]/a').click()  #
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="seat_grade_96160"]/a/div/span[2]/span[1]'))) # 블루존
+        WebDriverWait(driver, 300).until(EC.presence_of_element_located((By.XPATH, '//*[@id="seat_grade_96160"]/a/div/span[2]/span[1]'))) # 블루존
         seat01 = int(driver.find_element(By.XPATH, '//*[@id="seat_grade_96160"]/a/div/span[2]/span[1]').text) #블루존
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="seat_grade_96161"]/a/div/span[2]/span[1]'))) # 내야지정석
-        seat02 = int(driver.find_element(By.XPATH, '//*[@id="seat_grade_96161"]/a/div/span[2]/span[1]').text) #블루존
+        seat02 = int(driver.find_element(By.XPATH, '//*[@id="seat_grade_96161"]/a/div/span[2]/span[1]').text) #3루내야지장석
+        seat04 = int(driver.find_element(By.XPATH, '//*[@id="seat_grade_96165"]/a/div/span[2]/span[1]').text) #스카이하단
+        seat05 = int(driver.find_element(By.XPATH, '//*[@id="seat_grade_96166"]/a/div/span[2]/span[1]').text) #스카이상단
+        
         
         
         print('자리체크1')
         if seat01 > 1 :
-            print("블루존 자리발생")
+            print("블루존자리발생")
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="seat_grade_96160"]/div/div/span[1]/a'))) #자동배정
+            driver.find_element(By.XPATH, '//*[@id="seat_grade_96160"]/div/div/span[1]/a').click()
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[1]/form/fieldset/div/button[2]'))) # 인원추가
+            driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[1]/form/fieldset/div/button[2]').click()
+            driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[1]/form/fieldset/div/button[2]').click()
             play_mp3(mp3_file)
             time.sleep(10000)
-        elif seat01 > 1:
-            print("내야지정석 자리발생")
+        elif seat02 > 1 :
+            print("3루내야지장석 자리발생")
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="seat_grade_96161"]/div/div/span[1]/a'))) #자동배정
+            driver.find_element(By.XPATH, '//*[@id="seat_grade_96161"]/div/div/span[1]/a').click()
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[1]/form/fieldset/div/button[2]'))) # 인원추가
+            driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[1]/form/fieldset/div/button[2]').click()
+            driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[1]/form/fieldset/div/button[2]').click()
+            play_mp3(mp3_file)
+            time.sleep(10000)
+        elif seat04 > 1  :
+            print("스카이하단 자리발생")
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="seat_grade_96165"]/div/div/span[1]/a'))) #자동배정
+            driver.find_element(By.XPATH, '//*[@id="seat_grade_96165"]/div/div/span[1]/a').click()
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[1]/form/fieldset/div/button[2]'))) # 인원추가
+            driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[1]/form/fieldset/div/button[2]').click()
+            driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[1]/form/fieldset/div/button[2]').click()
+            play_mp3(mp3_file)
+            time.sleep(10000)
+        elif seat05 > 1  :
+            print("스카이상단 자리발생")
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="seat_grade_96166"]/div/div/span[1]/a'))) #자동배정
+            driver.find_element(By.XPATH, '//*[@id="seat_grade_96166"]/div/div/span[1]/a').click()
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[1]/form/fieldset/div/button[2]'))) # 인원추가
+            driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[1]/form/fieldset/div/button[2]').click()
+            driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[2]/div[5]/div[1]/form/fieldset/div/button[2]').click()
             play_mp3(mp3_file)
             time.sleep(10000)
         else :
-             print(f"블루존자리: {seat01}")
-             print(f"내야지정석: {seat02}")
-             print("자리없음")
-             time.sleep(0.5)
+            print(f"삼성자리: {seat01}, {seat02}, {seat04}, {seat05}")
+            print("자리없음")
+        time.sleep(0.5)
     except Exception as e:
         print(f'오류 발생: {e}')
-        # try : 
-        #     WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.XPATH, '//*[@id="seat_grade_95525"]/a/div/span[1]'))) #
-        #     driver.find_element(By.XPATH, '//*[@id="seat_grade_95525"]/a/div/span[1]').click()  #
-        #     WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.XPATH, '//*[@id="seat_grade_95525"]/div/div/span/a'))) #
-        #     driver.find_element(By.XPATH, '//*[@id="seat_grade_95525"]/div/div/span/a').click()  #
-            
-        # except:
-        #     print('')
         check()
 
 
 def macro02():
     print("시작")
-    WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.XPATH, '//*[@id="seat_grade_96160"]/a/div/span[2]/span[1]'))) #
-    seat01 = int(driver.find_element(By.XPATH, '//*[@id="seat_grade_96160"]/a/div/span[2]/span[1]').text)
 
-    if seat01 == 0 :
+    WebDriverWait(driver, 300).until(EC.presence_of_element_located((By.XPATH, '//*[@id="seat_grade_96160"]/a/div/span[2]/span[1]'))) # 블루존
+    seat01 = int(driver.find_element(By.XPATH, '//*[@id="seat_grade_96160"]/a/div/span[2]/span[1]').text) #블루존
+    seat02 = int(driver.find_element(By.XPATH, '//*[@id="seat_grade_96161"]/a/div/span[2]/span[1]').text) #3루내야지장석
+    seat04 = int(driver.find_element(By.XPATH, '//*[@id="seat_grade_96165"]/a/div/span[2]/span[1]').text) #스카이하단
+    seat05 = int(driver.find_element(By.XPATH, '//*[@id="seat_grade_96166"]/a/div/span[2]/span[1]').text) #스카이상단
+
+    if seat01 == 0 and seat02 == 0 and seat04 == 0 and seat05 == 0 :
             print("자리없음")
             each01 = 0
             while each01 < 1 :              
                 check()
     else :
         print("자리발생")
-        play_mp3(mp3_file)
         time.sleep(10000)
           
 def macro01():
